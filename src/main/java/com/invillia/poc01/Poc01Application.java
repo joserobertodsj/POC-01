d36cd54e4b3783ec42e161671f2d2c42da9fcae8
José Roberto Jr <joserobertodsj@gmail.com>
2022-12-12 14:47:37 -0300

    Subindo estrutura da aplicação

---
+++ src/main/java/com/invillia/poc01/Poc01Application.java
@@ -0,0 +1,13 @@
+package com.invillia.poc01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Poc01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Poc01Application.class, args);
+	}
+
+}
