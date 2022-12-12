dc301120125b3fd7c71553e2f56534fbdea85b0a
José Roberto Jr <joserobertodsj@gmail.com>
2022-12-12 16:38:56 -0300

    Atualizando entidade Address

--- src/main/java/com/invillia/poc01/models/Address.java
+++ src/main/java/com/invillia/poc01/models/Address.java
@@ -1,4 +1,46 @@
 package com.invillia.poc01.models;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tb_address")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idAddress;
+
+    @NotBlank(message = "Preenchimento obrigatório!")
+    private String street;
+
+    @NotBlank(message = "Preenchimento obrigatório!")
+    private String number;
+
+    @NotBlank(message = "Preenchimento obrigatório!")
+    private String district;
+
+    @NotBlank(message = "Preenchimento obrigatório!")
+    private String city;
+
+    @NotBlank(message = "Preenchimento obrigatório!")
+    private String state;
+
+    @NotBlank(message = "Preenchimento obrigatório!")
+    private String zipCode;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+    @NotEmpty(message = "Preenchimento obrigatório!")
+    private Boolean mainAddress;
+
+
 }
