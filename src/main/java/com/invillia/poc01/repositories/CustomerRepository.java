f1349bcfc8ee2b989744079bcd678f62190d6f19
José Roberto Jr <joserobertodsj@gmail.com>
2023-01-04 12:42:57 -0300

    Add método no repository

--- src/main/java/com/invillia/poc01/repositories/CustomerRepository.java
+++ src/main/java/com/invillia/poc01/repositories/CustomerRepository.java
@@ -1,6 +1,8 @@
 package com.invillia.poc01.repositories;
 
 import com.invillia.poc01.models.CustomerModel;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +11,8 @@ public interface CustomerRepository extends JpaRepository<CustomerModel, Long> {
     boolean existsByEmail(String email);
 
     boolean existsByDocumentNumber(String documentNumber);
+
+    Page<CustomerModel> findByNameIgnoreCase (String name, Pageable pageable);
+
 
 }
