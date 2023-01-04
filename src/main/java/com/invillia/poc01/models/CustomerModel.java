48bdf88812637c03345dffbd45d7cd0efbe6ac02
José Roberto Jr <joserobertodsj@gmail.com>
2023-01-04 10:31:11 -0300

    Aplicando validação no CustomerModel

--- src/main/java/com/invillia/poc01/models/CustomerModel.java
+++ src/main/java/com/invillia/poc01/models/CustomerModel.java
@@ -1,12 +1,19 @@
 package com.invillia.poc01.models;
 
-import com.invillia.poc01.enums.DocumentType;
+import com.invillia.poc01.enums.DocumentType;;
+import com.invillia.poc01.validation.groupValidation.CnpjGroup;
+import com.invillia.poc01.validation.groupValidation.CpfGroup;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
+
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.br.CNPJ;
+import org.hibernate.validator.constraints.br.CPF;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,9 +36,13 @@ public class CustomerModel {
     private DocumentType documentType;
 
     @NotBlank(message = "Preenchimento obrigatório!")
+    @CPF(groups = CpfGroup.class)
+    @CNPJ(groups = CnpjGroup.class)
+    @Pattern(regexp = "([0-9]{2}[\\.]?[0-9]{3}[\\.]?[0-9]{3}[\\/]?[0-9]{4}[-]?[0-9]{2})|([0-9]{3}[\\.]?[0-9]{3}[\\.]?[0-9]{3}[-]?[0-9]{2})", message = "Informe um CPF ou CNPJ válido!")
     private String documentNumber;
 
     @NotBlank(message = "Preenchimento obrigatório!")
+    @Email(message = "E-mail inválido.")
     private String email;
 
     @NotBlank(message = "Preenchimento obrigatório!")
